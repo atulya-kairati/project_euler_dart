a03b01c71b8a8b8b9aa2b9890ae4bf7def50aea9
atulya-kairati <atulyakairati@gmail.com>
2021-03-21 15:32:32 +0530

    p 21

---
+++ problem_21.dart
@@ -0,0 +1,106 @@
+// Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+// If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+// Evaluate the sum of all the amicable numbers under 10000.
+import "dart:math" as math;
+
+int getSum(List<int> numbers){
+  // calculates sum by excluding last element
+  if (numbers.length <= 2)
+    return numbers.first;
+  numbers.removeAt(numbers.length -1);
+  return numbers.reduce(
+    (value, element) => value + element,
+  );
+}
+
+Map<int, int> getPrimeFactors(int number){
+  Map<int, int> factors = {};
+  var div = 2;
+  while (number > 1){
+    while (number % div == 0){
+      factors.update(
+        div,
+        (prevVal) => ++prevVal,
+        ifAbsent: () => 1,
+      );
+      number ~/= div;
+    }
+    div++;
+  }
+  return factors;
+}
+
+List<List<int>> getHelperArray(Map<int, int> primeFactors){
+  // create a array with dimensions equal to the no of
+  // distinct prime factors, this array can be used to
+  // find all divisors
+  List<List<int>> arr = [];
+  primeFactors.forEach((factor, exponent){
+    List<int> temp = [];
+    for (int i = 0; i <= exponent; i++){
+      temp.add(math.pow(factor, i).toInt());
+    }
+    arr.add(temp);
+  });
+  return arr; 
+}
+
+List<int> getCombination(List<int> list1, List<int> list2){
+  List<int> temp = [];
+  for (var i in list1){
+    for (var j in list2){
+      temp.add(i*j);
+    }
+  }
+  return temp;
+}
+
+List<int> getDivisors(int number){
+  var pfs = getPrimeFactors(number);
+  var arr = getHelperArray(pfs);
+  var accumulator = arr.first;
+  for (var i = 1, len = arr.length; i < len; ++i) {
+    accumulator = getCombination(arr[i], accumulator);
+  }
+  return accumulator;
+}
+
+void main() {
+  var limit = 10000;
+  var sumofAmicable = 0;
+  List<int> amicableNumbers = [];
+  for (int i = 10; i <= limit; i++){
+    var divsOfi = getDivisors(i);
+    
+    if(divsOfi.length <= 2)
+      // this means i is prime and can't be 
+      // processed further
+      continue;
+    var temp = getSum(divsOfi);
+    
+    // cond. for amicable Numbers
+    if(i == temp) continue;
+
+    var divsOfTemp = getDivisors(temp);
+    
+    if(divsOfTemp.length <= 2)
+      // this means temp is prime and can't be 
+      // processed further
+      continue;
+    var n = getSum(divsOfTemp);
+    // print(n);
+
+    if(i == n){
+      amicableNumbers.add(i);
+      print(i);
+    }
+  }
+  
+  var sum = amicableNumbers.reduce(
+      (value, element) => value + element
+    );
+  print(sum);
+}
